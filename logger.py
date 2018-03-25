49694373fab750004a4916bdd07e91d73fd5fe8c
JuHyun Lee <leejaycoke@gmail.com>
2018-03-25 14:50:46 +0900

    로거 기능 추가

--- logger.py
+++ logger.py
@@ -2,13 +2,33 @@ import os
 import logging
 import logging.handlers
 
-if not os.path.exists('./log'):
-    os.mkdirs('./log')
+LOGGER_NAME = "gz-logger"
+LOG_FORMAT = "[%(asctime)s] [%(levelname)s] [%(filename)s:%(lineno)s] %(message)s"
+LOG_FILE_PATH = os.getcwd() + '/log'
 
 
-log = logging.getLogger('gz-logger')
-fomatter = logging.Formatter(
-    '[%(levelname)s|%(filename)s:%(lineno)s] %(asctime)s > %(message)s')
+def get_log_path():
+    if not os.path.isdir(LOG_FILE_PATH):
+        os.path.makedirs(LOG_FILE_PATH)
+    return LOG_FILE_PATH + '/gz.log'
 
-fileHandler = logging.FileHandler('./log/gz.log')
-fileHandler.setFormatter(fomatter)
+
+def get_log_level():
+    return logging.DEBUG if os.environ.get('GZ_ENV', 'LOCAL') == 'LOCAL' \
+        else logging.INFO
+
+
+def create_logger():
+    log = logging.getLogger(LOGGER_NAME)
+    fomatter = logging.Formatter(LOG_FORMAT)
+
+    file_handler = logging.FileHandler(get_log_path())
+    file_handler.setFormatter(fomatter)
+
+    log.addHandler(file_handler)
+    log.setLevel(get_log_level())
+
+    return log
+
+
+log = create_logger()
